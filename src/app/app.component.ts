8adc619825601bd90813e4b5c80f6fa1a055ddda
AJ Hephner <ajhephner178@gmail.com>
2017-10-10 09:27:04 -0400

    ajh-added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `
+    <div>
+        <nav class='navbar navbar-default' style="background-color: #FF7575">
+            <div class='container-fluid'>
+                <a class='navbar-brand'style="color: white">{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']" style="color: white">Home</a></li>
+                    <li><a [routerLink]="['/signup']"style="color: white">Sign Up</a></li>
+                    <li><a [routerLink]="['/products']"style="color: white">Menu</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+    `
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientJsonpModule } from '@angular/common/http';
+import { RouterModule, Routes} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
+const appRoutes: Routes = [
+  {path: 'test', component: TestComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientJsonpModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
