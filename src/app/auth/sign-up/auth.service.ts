fe5e3a93b162302f02c863ef9b264a12914058d9
Madyson Ward <MadysonWard94@gmail.com>
2017-10-11 11:09:30 -0400

    MW - Added validation for signing up. Alerts an error message for invalid email or if email is already registered, and if password is too weak

--- src/app/auth/sign-up/auth.service.ts
+++ src/app/auth/sign-up/auth.service.ts
@@ -3,7 +3,22 @@ export class AuthService {
     signupUser(email: string, password: string){
       firebase.auth().createUserWithEmailAndPassword(email, password)
         .catch(
-            error => console.log(error)
+            error => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode == "auth/email-already-in-use"){
+                    alert("Email already registered.");
+                }
+                if (errorCode == "auth/invalid-email"){
+                    alert("Please enter a valid email.");
+                }
+                if (errorCode == "auth/weak-password"){
+                    alert("You must enter a stronger password.");
+                } else {
+                    alert(errorMessage);
+                } 
+                console.log(error);
+            }         
         )
     }
 }
