ab8282c5c6d00768d228d0e3447ebef9c2757e1e
Isaac Ludlow <isaac@theludlows.org>
2017-10-26 17:12:19 -0400

    Added lots of things

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -20,7 +20,7 @@ export class SignUpComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  company = ["Eli Lily", "Roche", "Northwind"]
+  company = ["Eli Lilly", "Roche", "Northwind"]
   location = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", 
   "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
--- src/app/leader-boards/leader-boards.component.ts
+++ src/app/leader-boards/leader-boards.component.ts
@@ -1,8 +1,10 @@
-import { LeaderService } from './leader.service';
+import {SignupService} from '../auth/signup.service';
 import {Component, OnInit} from '@angular/core';
-import { AngularFireDatabaseModule, AngularFireDatabase } from "angularfire2/database";
+import {NgForm, Form} from '@angular/forms';
+import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireObject } from "angularfire2/database";
 import * as firebase from "firebase";
 import {Observable} from "rxjs/RX";
+import {AuthService} from "../auth/auth.service";
 
 @Component({
   selector: 'app-leader-boards',
@@ -10,51 +12,48 @@ import {Observable} from "rxjs/RX";
   styleUrls: ['./leader-boards.component.css']
 })
 
-
 export class LeaderBoardsComponent implements OnInit {
-userList = [];
-
-constructor(private leaderService: LeaderService ){}
-
-// getUsers(
-//   email: string, 
-//   fname: string,
-//   lname: string,
-// ){
-//   this.userList.push({
-//     email : email,
-//     fname : fname,
-//     lname : lname
-//   });
-// }
+  user: any[];
 
 
+ constructor(private signupservice: SignupService,
+  private db: AngularFireDatabase){}
+    ngOnInit() {
+      this.signupservice.getUsers()
+      .subscribe(
+        user => {
+          this.user = user }),
+          
+         
+  
+       (error) => console.log(error)
+    
+    }
+  
 
-  nCnt: number = 0;
+
+
+
+ 
+
+
+ nCnt: number = 0;
   clickMe(){
       this.nCnt = this.nCnt + 1;
-      
-
       if(this.nCnt === 3){
        alert('3')
       }
-     
-    }
-
-getUser(){
-  this.leaderService.showUsers()
-  .then(
-
-(userList: any[]) => console.log(userList),
-    (error) => console.log(error)
-  );
-}
-  ngOnInit() {
-    this.leaderService.showUsers()
-    .then(
-  
-  (userList: any[]) => console.log(userList),
-      (error) => console.log(error)
-    );
   }
+  // getUser(){
+  //   this.signupservice.getUsers()
+  //   .subscribe(
+  //     (user: any) => console.log(user[0].company),
+  //     // (user: any) => console.log(user.data.company),
+  //     (error) => console.log(error)
+  //   );
+  // }
+  
+
+ 
+ 
 }
