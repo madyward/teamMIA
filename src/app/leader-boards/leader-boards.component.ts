621db8159609db7b7ac57d9c727155a8dd011790
Madyson Ward <MadysonWard94@gmail.com>
2017-10-27 09:02:33 -0400

    MW - cleaned code

--- src/app/leader-boards/leader-boards.component.ts
+++ src/app/leader-boards/leader-boards.component.ts
@@ -3,8 +3,6 @@ import {NgForm, Form} from '@angular/forms';
 import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireObject } from "angularfire2/database";
 import * as firebase from "firebase";
 import {Observable} from "rxjs/RX";
-import * as firebase from "firebase";
-import {AngularFireDatabaseModule, AngularFireDatabase, AngularFireList, AngularFireObject} from "angularfire2/database";
 import {AuthService} from "../auth/auth.service";
 import {SignupService} from '../auth/signup.service';
 
