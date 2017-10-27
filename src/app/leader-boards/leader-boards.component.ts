99e045444f7a5ee747a5a0ea03fbf5058f82dd36
Isaac Ludlow <isaac@theludlows.org>
2017-10-27 09:08:57 -0400

    Copy paste from github

--- src/app/leader-boards/leader-boards.component.ts
+++ src/app/leader-boards/leader-boards.component.ts
@@ -1,7 +1,6 @@
 import {SignupService} from '../auth/signup.service';
 import {Component, OnInit} from '@angular/core';
-import {NgForm, Form} from '@angular/forms';
-import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireObject } from "angularfire2/database";
+import { AngularFireDatabaseModule, AngularFireDatabase } from "angularfire2/database";
 import * as firebase from "firebase";
 import {Observable} from "rxjs/RX";
 import {AuthService} from "../auth/auth.service";
@@ -13,37 +12,30 @@ import {AuthService} from "../auth/auth.service";
 })
 
 export class LeaderBoardsComponent implements OnInit {
-  user: any[];
+userList = [];
 
+constructor(private signupservice: SignupService ){}
 
- constructor(private signupservice: SignupService,
-  private db: AngularFireDatabase){}
-    ngOnInit() {
-      this.signupservice.getUsers()
-      .subscribe(
-        user => {
-          this.user = user }),
-          
-         
-  
-       (error) => console.log(error)
-    
-    }
-  
-
-
-
+  nCnt: number = 0;
+  clickMe(){
+      this.nCnt = this.nCnt + 1;
+      if(this.nCnt === 3){
+       alert('3')
+      }
   }
-  // getUser(){
-  //   this.signupservice.getUsers()
-  //   .subscribe(
-  //     (user: any) => console.log(user[0].company),
-  //     // (user: any) => console.log(user.data.company),
-  //     (error) => console.log(error)
-  //   );
-  // }
-  
-
- 
- 
+  getUser(){
+    this.signupservice.getUsers()
+    .subscribe(
+      (user: any) => console.log(user[0].company),
+      // (user: any) => console.log(user.data.company),
+      (error) => console.log(error)
+    );
+  }
+  ngOnInit() {
+    this.signupservice.getUsers()
+    .subscribe(
+      (user: any[]) => console.log(user),
+      (error) => console.log(error)
+    );
+  }
 }
